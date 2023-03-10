98a1fb43d80b223e81ec7031882c1acd2884714f
Ubuntu <ubuntu@ip-172-31-2-216.ec2.internal>
2023-03-10 03:23:34 +0000

    Prueba Final

--- leer_archivo.py
+++ leer_archivo.py
@@ -1,32 +1,31 @@
-import json
 import boto3
 from datetime import datetime
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
 def get_url():
-    return "https://casas.mitula.com.co/searchRE/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/orden-0/op-1/m2_min-1/m2_max-200/hab_min-1/ban_min-1/q-bogot%C3%A1?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
+    return "https://casas.mitula.com.co/searchRE" + \
+           "/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/" + \
+           "orden-0/op-1/m2_min-1/m2_max-200/hab_min-1/" + \
+           "ban_min-1/q-bogot%C3%A1?req_sgmt=" + \
+           "REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
+
 
 def get_boto():
     return boto3.client('s3')
-    
+
+
 def get_date():
     return datetime.today().strftime('%Y-%m-%d')
-    
+
 
 def f():
-    
     fecha_actual = get_date()
-
     url = get_url()
-
-    
     with urlopen(url) as response:
         html = response.read()
-        
     s3 = get_boto()
-        
-    s3.put_object(Body=html,Bucket='landing-casas-20020503',Key= str(fecha_actual) + ".html")
-    
+    s3.put_object(Body=html,
+                  Bucket='landing-casas-20020503',
+                  Key=str(fecha_actual)+".html")
     return "hola"
--- test_leer_archivo.py
+++ test_leer_archivo.py
@@ -2,6 +2,7 @@ import boto3
 from datetime import datetime
 from leer_archivo import get_url, get_boto, get_date
 
+
 def test_get_boto(mocker):
     mocker.patch("boto3.client")
     s3_mock = mocker.MagicMock()
@@ -9,11 +10,15 @@ def test_get_boto(mocker):
     s3 = get_boto()
     assert s3 == s3_mock
 
+
 def test_get_date():
     dt = get_date()
     assert dt == datetime.today().strftime('%Y-%m-%d')
-    
+
 
 def test_get_url():
-    assert get_url() == "https://casas.mitula.com.co/searchRE/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/orden-0/op-1/m2_min-1/m2_max-200/hab_min-1/ban_min-1/q-bogot%C3%A1?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
-    
+    assert get_url() == "https://casas.mitula.com.co/searchRE" + \
+                        "/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/" + \
+                        "orden-0/op-1/m2_min-1/m2_max-200/hab_min-1/" + \
+                        "ban_min-1/q-bogot%C3%A1?req_sgmt=" + \
+                        "REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
